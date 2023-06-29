23984407f3d1a54c099afbd4a231422f6d0e13bf
Burness Scott III <bsiii102798@gmail.com>
2023-06-28 18:23:04 -0700

    expressNote

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const html = require('./routes/html-routes')
+const api = require('./routes/api-routes')
+const PORT = 
+//process.env.PORT || 
+3005;
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html)
+app.use(api)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
