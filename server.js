8a16314e385d99ed05402408c7e00d3e4cafff27
Burness Scott III <bsiii102798@gmail.com>
2023-08-27 19:04:15 -0700

    stuff

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", html)
-app.use("/api", api)
+app.use(html)
+app.use(api)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
